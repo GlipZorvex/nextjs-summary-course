e902677643722bbfc7b2c46af9a6a8be0aaf1e45
Vladislav Ladan <github.pwsw8k0tr4va27apw3sc@ttflux.com>
2023-08-12 01:11:38 +0200

    add pages

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Sukiennice_and_Main_Market_Square_Krakow_Poland.JPG/1920px-Sukiennice_and_Main_Market_Square_Krakow_Poland.JPG",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image: "https://www.european-games.org/wp-content/uploads/2022/12/Krakow-Mainsquare-st-mary-s-basilica-in-main-square-of-krakow-wawel-castle-historic-center-city-with-ancient-architecture.jpeg",
+    address: "Some address 10, 54321 Some City",
+    description: "This is a Second meetup!",
+  },
+]
+
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS}/>
+}
