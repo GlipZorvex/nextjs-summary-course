c5dc4c2c9e3b08573e18ebfabbcc38856cc20f52
Vladislav Ladan <github.pwsw8k0tr4va27apw3sc@ttflux.com>
2023-08-12 16:42:14 +0200

    add MeetupDetails component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+export default function MeetupDetailsPage() {
+  return <MeetupDetails
+    image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Sukiennice_and_Main_Market_Square_Krakow_Poland.JPG/1920px-Sukiennice_and_Main_Market_Square_Krakow_Poland.JPG"
+    title="A First Meetup"
+    address="Some address 5, 12345 Some City"
+    description="This is a first meetup!"/>
+}
