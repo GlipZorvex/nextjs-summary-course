208a3b52c54fe87f8b3c453aaa0aa6128a097589
Vladislav Ladan <github.pwsw8k0tr4va27apw3sc@ttflux.com>
2023-08-13 18:58:48 +0200

    add mongodb to work with nextjs api routes

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,22 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import {useRouter} from "next/router";
 
 export default function NewMeetupPage() {
-  function handleAddMeetup(meetup) {
+  const router = useRouter();
 
+  async function handleAddMeetup(meetup) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetup),
+      headers: {
+        "Content-type": "application/json",
+      }
+    });
+
+    const data = await response.json();
+    console.log(`data: ${data}`);
+
+    router.replace("/");
   }
-
   return <NewMeetupForm onAddMeetup={handleAddMeetup}/>
 }
